1ad40bf35ebe4800b9a5d57614f74472f22e8bb2
lchen104 <lchen@zdmedia.com>
2023-10-02 10:37:25 -0400

    Added Fibonacci Algorithm

---
+++ README.md
@@ -0,0 +1,6 @@
+# Introduction
+Learning basic Algorithms and understanding their time complexity
+
+## Algorithms
+- Fibonacci (Find fibonacci number on index of n)
+- Primality (Returns true if n is a prime number)
---
+++ primality.js
@@ -0,0 +1,4 @@
+// A Prime number is ONLY divisible by itself or by 1
+// Both 5 and 9 are divisible by itself or by 1, BUT 9 is also divisible by 3 therefore, 9 is NOT a prime number
+// While dividing, n must return a whole INTERGER and no REMAINDER (ie. 1, 2, 3) NOT (ie. 1.2, 1.4, 1.6)
+
