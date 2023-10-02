0f2a7ea00ae8a36254dbe298e11fa2d29c194e4a
lchen104 <lchen@zdmedia.com>
2023-10-02 18:41:07 -0400

    Improved Primality Algorithm by squaring n

--- primality.js
+++ primality.js
@@ -3,21 +3,28 @@
 // While dividing, n must return a whole INTERGER and no REMAINDER (ie. 1, 2, 3) NOT (ie. 1.2, 1.4, 1.6)
 
 const isPrime = (n) => {
-    for (let i = 2; i < n; i++) {
-        console.log(i + ':' + n)
-        if (n % i === 0) {
-            console.log(n + ' divided by ' + i + ' = 0') 
-            return false;
+    for (let i = 2; i < Math.sqrt(n); i++) { // 1
+        // console.log(i + ':' + n)
+        console.log('Runnning')
+        if (n % i === 0) { // n
+            // console.log(n + ' divided by ' + i + ' = 0')
+            return false; // 1
         }
         
     }
-    return true;
+    return true; // 1
 }
+
+// Best case: n = 1 OR n = 2 => O(1)
+// Average case: O(n) / (Imporved: O(sqrt(n)))
+// Worst case: n = 27,277 => O(n) / (Imporved: O(sqrt(n)))
+
 
 // console.log(isPrime(3));
 // console.log(isPrime(4));
-console.log(isPrime(5));
+// console.log(isPrime(5));
 // console.log(isPrime(6));
 // console.log(isPrime(7));
 // console.log(isPrime(8));
-console.log(isPrime(9));
+// console.log(isPrime(9));
+console.log(isPrime(27277)); // A prime number
