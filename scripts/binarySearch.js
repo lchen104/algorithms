f4f1f6346db8c3a726512ee460ffc6d34bf5d640
lchen104 <lchen@zdmedia.com>
2023-10-12 11:56:16 -0400

    Added addtional comments to clearify binarySearch

--- scripts/binarySearch.js
+++ scripts/binarySearch.js
@@ -2,23 +2,35 @@
 
 const binarySearch = (sortedArr, element) => {
     let startIndex = 0;
+
+    // get the last index of the array
     let endIndex = sortedArr.length - 1;
 
     while (startIndex <= endIndex) {
         console.log('RUNNING');
+        
+        // Finding and Setting the middleIndex of the current array by subtracting endIndex from startIndex
         const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+        // console.log('MidIndex: ' + middleIndex);
 
+        // If element searching for is found, return the middleIndex and exit
         if (element === sortedArr[middleIndex]) {
             return middleIndex;
         }
 
+        // If the current middleindex value in the array is less than the element searched for, 
         if (sortedArr[middleIndex] < element) {
+            // set startIndex to the middleIndex + 1
             startIndex = middleIndex + 1;
         } else {
+            // set endIndex to middleIndex - 1
             endIndex = middleIndex - 1;
         }
     }
 }
+
+// O(1) => Constant Time Complexity
+// Array MUST be pre-sorted first befoe running binary search
 
 const arr = [1, 5, 9, 13, 99, 100];
 
