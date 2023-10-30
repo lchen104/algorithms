ef715f6fd253c869c3ea84f9ec23f90cad0f61b4
lchen104 <lchen@zdmedia.com>
2023-10-30 17:04:18 -0400

    Added Recursion Algorithm

---
+++ scripts/recursion.js
@@ -0,0 +1,53 @@
+// Recursion to print John's children and his grandchildrens, great grandchildrens
+
+const tree = {
+    name: 'John',
+    children: [
+        {
+            name: 'Jim',
+            children: []
+        },
+        {
+            name: 'Zoe',
+            children: [
+                {
+                    name: 'Kyle',
+                    children: [
+                        {
+                            name: 'Mike',
+                            children: [
+                                {
+                                    name: 'Todd',
+                                    children: []
+                                },
+                                {
+                                    name: 'Andy',
+                                    children: []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: 'Sophia',
+                    children: []
+                }
+            ]
+        }
+    ]
+}
+
+const printChildrenRecursive = (t) => {
+    // return and exit if no children
+    if (t.children.length <= 0) {
+        return;
+    }
+
+    t.children.forEach(child => {
+        console.log(child.name);
+        printChildrenRecursive(child);
+    })
+}
+
+
+console.log(printChildrenRecursive(tree));
